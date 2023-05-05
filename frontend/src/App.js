919d6a877c23f0ebd57cc4a7b155fa68a6b6f944
Mathieu Girard <mgirard514@gmail.com>
2023-05-05 14:32:35 -0400

    Ajout d'un footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,26 +4,33 @@ import DeroulementStagiaireScreen from "./Screens/DeroulementStagiaireScreen";
 import FAQScreen from "./Screens/FAQScreen";
 import FormulaireEmployeurScreen from "./Screens/FormulaireEmployeurScreen";
 import ProfilsCompetencesScreen from "./Screens/ProfilsCompetencesScreen";
-import MainNavigation from "./Navigation/MainNavigation";
-
+import MainNavigation from "./shared/Navigation/MainNavigation";
+import Footer from "./shared/Footer";
+import React from "react";
 function App() {
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<AccueilScreen />} />
+    <React.Fragment>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<AccueilScreen />} />
 
-          <Route path="/FAQ" element={<FAQScreen />} />
+            <Route path="/FAQ" element={<FAQScreen />} />
 
-          <Route path="/Profils" element={<ProfilsCompetencesScreen />} />
+            <Route path="/Profils" element={<ProfilsCompetencesScreen />} />
 
-          <Route path="/Deroulement" element={<DeroulementStagiaireScreen />} />
+            <Route
+              path="/Deroulement"
+              element={<DeroulementStagiaireScreen />}
+            />
 
-          <Route path="/Employeur" element={<FormulaireEmployeurScreen />} />
-        </Routes>
-      </main>
-    </Router>
+            <Route path="/Employeur" element={<FormulaireEmployeurScreen />} />
+          </Routes>
+        </main>
+      </Router>
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/shared/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+      <div class="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+        <div>
+          <h6 class="mb-4 flex justify-center font-semibold uppercase">
+            Contact
+          </h6>
+          <p class="mb-4 flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="mr-3 h-5 w-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            Sylvain Labranche
+          </p>
+          <p class="mb-4 flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="mr-3 h-5 w-5"
+            >
+              <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
+              <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+            </svg>
+            <a href="mailto:sylvain.labranche@cmontmorency.qc.ca">
+              sylvain.labranche@cmontmorency.qc.ca
+            </a>
+          </p>
+        </div>
+        <span>Dernière modification: ...</span>
+      </div>
+    </footer>
+  );
+}
